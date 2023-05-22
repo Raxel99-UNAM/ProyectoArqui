a5dc3d64960b5c9b90f9dfa04ad3f8026fe72e0c
Raymundo Mondragon <rmondragonlara@comunidad.unam.mx>
2023-05-22 15:12:17 -0600

    Api maps

--- Vistas/espaciosUrbanos.php
+++ Vistas/espaciosUrbanos.php
@@ -1,81 +1,53 @@
 <?php
-require_once '../BaseDatos/db_connect.php'; // Asegúrate de proporcionar la ruta correcta al archivo db_connect.php
+$conexion = mysqli_connect("localhost", "root", "", "arquitectura");
+if ($conexion->connect_error) {
+    die("La conexion falló: " . $conexion->connect_error);
+}
 
-$query = "SELECT * FROM espacios_urbanos ORDER BY fecha_creacion DESC";
-$result = mysqli_query($conn, $query);
+$sql = "SELECT * FROM espacios_urbanos ORDER BY nombre";
+$result = $conexion->query($sql);
 ?>
-
 <!DOCTYPE html>
-<html lang="es">
+<html>
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Espacios Urbanos</title>
-
-    <link rel="stylesheet" href="../Diseño/styles.css">
-    
-    <!-- Fuentes Google -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
-
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="..\favicon\apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="..\favicon\favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="..\favicon\favicon-16x16.png">
-    <link rel="manifest" href="site.webmanifest">
-        
+    <link rel="stylesheet" href="../estilos.css">
+    <!-- Aquí incluimos la librería de Google Maps -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDP7dRMjVGQuZ5BpzxGvJfsr50UI_-zTTI"></script>
 </head>
 
 <body>
-    <div class="content-wrapper">
-        <div class="header-bg">
-            <header class="header container">
-        
-                <div class="titulo-contenedor">
-                    <h1 class="titulo">Espacios Urbanos</h1>
-                </div>
-        
-                <div class="navegacion">
-                    <ul class="links">
-                        <li class="link"><a href="inicio.html">Inicio</a></li>
-                        <li class="link"><a href="edificios.php">Edificios</a></li>
-                        <li class="link"><a href="espaciosUrbanos.php">Espacios Urbanos</a></li>
-                        <li class="link"><a href="biografias.php">Biografías</a></li>
-                    </ul>
-                </div>
-        
-            </header>
-        </div>
-
-
-        <!-- Información Espacio Urbano -->
-
-        <div class="espacio container">
-
-            <div class="decoracion">
-                <div class="circulo">
-
-                </div>
-            </div>
-
-            <div class="informacion__principal seccion">
-            <?php
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<div class='espacio'>";
-                    echo "<h2>" . $row['nombre'] . "</h2>";
-                    echo "<p>Descripción del proyecto original: " . $row['descripcion_proyecto_original'] . "</p>";
-                    echo "<p>Ubicación: " . $row['ubicacion'] . "</p>";
-                    echo "<p>Contexto histórico: " . $row['contexto_historico'] . "</p>";
-                    echo "<p>Transformaciones: " . $row['transformaciones'] . "</p>";
-                    echo "</div>";
-                }
-                ?>
+    <?php
+    while ($espacio_urbano = mysqli_fetch_array($result)) {
+    ?>
+        <div class="card">
+            <div class="card-content">
+                <h2><?php echo $espacio_urbano['nombre']; ?></h2>
+                <p><?php echo $espacio_urbano['funcion']; ?></p>
+                <p><?php echo $espacio_urbano['año_establecimiento']; ?></p>
+                <!-- Aquí creamos el div donde se mostrará el mapa -->
+                <div id="mapa_<?php echo $espacio_urbano['id']; ?>" style="width: 100%; height: 400px;"></div>
+                <!-- Aquí creamos el script para mostrar el mapa -->
+                <script>
+                    var map = new google.maps.Map(document.getElementById('mapa_<?php echo $espacio_urbano['id']; ?>'), {
+                        zoom: 14,
+                        center: new google.maps.LatLng(<?php echo $espacio_urbano['latitud']; ?>, <?php echo $espacio_urbano['longitud']; ?>),
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    });
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(<?php echo $espacio_urbano['latitud']; ?>, <?php echo $espacio_urbano['longitud']; ?>),
+                        map: map
+                    });
+                </script>
             </div>
         </div>
-    </div>
-    
+    <?php
+    }
+    $conexion->close();
+    ?>
+
 <footer>
     <div class="Pie-pagina">
         <p>
