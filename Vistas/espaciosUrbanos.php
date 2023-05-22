9247996248462072752d3608ea61f8cd357ae154
Raymundo Mondragon <rmondragonlara@comunidad.unam.mx>
2023-05-22 16:53:23 -0600

    Ya jala el header de espaciosUrbanos.php

--- Vistas/espaciosUrbanos.php
+++ Vistas/espaciosUrbanos.php
@@ -12,8 +12,22 @@ $result = $conexion->query($sql);
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Espacios Urbanos</title>
+
+
     <link rel="stylesheet" href="../Diseño/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="..\favicon\apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="..\favicon\favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="..\favicon\favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+
     <!-- Aquí incluimos la librería de Google Maps -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDP7dRMjVGQuZ5BpzxGvJfsr50UI_-zTTI"></script>
 </head>
