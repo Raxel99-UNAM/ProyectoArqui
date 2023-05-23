d5b63008222e92a7e5ab767412ae3bf9e2148026
Raxel99-UNAM <rmondragonlara@comunidad.unam.mx>
2023-05-23 09:08:58 -0600

    Coreecion de errores en el login y css

--- Vistas/check_password.php
+++ Vistas/check_password.php
@@ -3,10 +3,11 @@ session_start();
 
 $correct_password = 'Juan'; // Deberás reemplazar esto con la contraseña que desees
 
-if ($_POST['Juan'] == $correct_password) {
+if (isset($_POST['password']) && $_POST['password'] == $correct_password) {
     $_SESSION['authenticated'] = true;
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 } else {
     header('Location: ' . $_SERVER['HTTP_REFERER'] . '?incorrect_password=true');
 }
 ?>
+
