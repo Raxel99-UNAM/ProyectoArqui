c9fff83693d0b07333c1121b92d2c52ed9e14826
Raxel99-UNAM <rmondragonlara@comunidad.unam.mx>
2023-05-23 15:36:55 -0600

    nuevos archivos para el proyecto

---
+++ Vistas/menu.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+
+    <link rel="stylesheet" href="../Diseño/styles.css">
+    
+    <!-- Fuentes Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon"> 
+</head>
+
+<body>
+    <div class="content-wrapper">
+        <div class="header-bg">
+            <header class="header container">
+        
+                <div class="titulo-contenedor">
+                    <h1 class="titulo">Nuevos datos</h1>
+                </div>
+        
+                <div class="navegacion">
+                    <ul class="links">
+                        <li class="link"><a href="inicio.html">Inicio</a></li>
+                        <li class="link"><a href="edificios.php">Edificios</a></li>
+                        <li class="link"><a href="espaciosUrbanos.php">Espacios Urbanos</a></li>
+                        <li class="link"><a href="biografias.php">Biografías</a></li>
+                        <li class="link"><a href="login.php">Iniciar Sesión</a></li>
+                    </ul>
+                </div>
+        
+            </header>
+        </div>
+
+        <main>
+            <section class="inicio container">
+                <h2 class="titulo">Bienvenido</h2>
+                <p>Bienvenido al sitio web Espacio Arquitectónico en México, donde encontrarás información sobre edificios, espacios urbanos y biografías de arquitectos destacados en México.</p>
+                <p>Explora las diferentes secciones para conocer más sobre la arquitectura y el diseño urbano en México:</p>
+            </section>
+        </main>
+    </div>
+
+    <footer>
+        <div class="Pie-pagina">
+            <p>
+                Lorem ipsum dolor, s
+            </p>
+        </div>
+    </footer>
+</body>
+
+</html>
